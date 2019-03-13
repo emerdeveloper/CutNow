04246fb2cd80a2a873a4527e94453547043cb3f5
Emerson Javid Gonzalez Morales <emegonza@Emersons-MacBook-Pro.local>
2019-03-13 18:17:55 -0500

    Transactions between fragments finished

--- app/src/main/java/co/com/emegonza/cutnow/MainActivity.kt
+++ app/src/main/java/co/com/emegonza/cutnow/MainActivity.kt
@@ -2,11 +2,53 @@ package co.com.emegonza.cutnow
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.widget.Button
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var place : Button? = null
+    private var map : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+        setUpEvents()
+    }
+
+    //Init Widgets
+    private fun init() {
+        place = findViewById(R.id.btn_places)
+        map = findViewById(R.id.btn_map)
+    }
+
+    //Setup Events
+    private fun setUpEvents() {
+        place?.setOnClickListener { showPlacesList() }
+        map?.setOnClickListener { showMap() }
+    }
+
+    private fun showPlacesList() {
+        addFragment(PlacesFragment(), false, "one")
+    }
+
+    private fun showMap() {
+        addFragment(MapsFragment(), false, "one")
+    }
+
+    //Methods
+    private fun addFragment(fragment: Fragment, addToBackStack: Boolean, tag: String) {
+        val manager = supportFragmentManager
+        val ft = manager.beginTransaction()
+
+        if (addToBackStack) {
+            ft.addToBackStack(tag)
+        }
+        ft.replace(R.id.container_frame, fragment, tag)
+        ft.commitAllowingStateLoss()
     }
 }
--- app/src/main/java/co/com/emegonza/cutnow/MapsFragment.kt
+++ app/src/main/java/co/com/emegonza/cutnow/MapsFragment.kt
@@ -2,8 +2,8 @@ package co.com.emegonza.cutnow
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Color
 import android.location.Criteria
 import android.location.Location
 import android.location.LocationListener
@@ -11,23 +11,22 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentTransaction
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesUtil
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
-import android.widget.Toast
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
-import com.google.android.gms.common.GooglePlayServicesUtil
 
-class MapsActivity : AppCompatActivity(),
+class MapsFragment : Fragment(),
     OnMapReadyCallback,
     GoogleMap.OnMyLocationButtonClickListener,
     GoogleMap.OnMyLocationClickListener,
@@ -35,66 +34,50 @@ class MapsActivity : AppCompatActivity(),
 
     private val MY_LOCATION_REQUEST_CODE = 1
     private lateinit var mMap: GoogleMap
-    var place : Button? = null
     var fragment : SupportMapFragment? = null
+    var contextActivity : Context? = null
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        contextActivity = context
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-
-        init()
-        setUpEvents()
-
-        if (isGooglePlayServicesAvailable())
-        fragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        fragment?.getMapAsync(this)
     }
 
-    //Init Widgets
-    fun init() {
-        place = findViewById(R.id.btn_places)
+     override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+         // Inflate the layout for this fragment
+         val rootView : View = inflater.inflate(R.layout.activity_maps, container, false)
+
+         if (isGooglePlayServicesAvailable())
+         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+         fragment = childFragmentManager
+             .findFragmentById(R.id.map) as SupportMapFragment
+         fragment?.getMapAsync(this)
+
+         return rootView
     }
-
-    //Setup Events
-    fun setUpEvents() {
-        place?.setOnClickListener { showPlacesList() }
-    }
-
-    private fun showPlacesList() {
-        setFragment(PlacesFragment())
-    }
-
-
-    private fun setFragment(fragment : Fragment) {
-        val fragmentManager: FragmentManager
-        val fragmentTransaction: FragmentTransaction
-
-        fragmentManager = supportFragmentManager
-        fragmentTransaction = fragmentManager.beginTransaction()
-        //val inboxFragment = PlacesFragment()
-        fragmentTransaction.replace(R.id.map,fragment)
-        fragmentTransaction.commit()
-    }
-
-
 
 
     //Events Maps
     fun enableMyLocationIfPermitted() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (ContextCompat.checkSelfPermission(contextActivity!!, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(
-                this,
+                (MainActivity()),
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
                 MY_LOCATION_REQUEST_CODE
             )
 
             // Show rationale and request permission.
             Toast.makeText(
-                this, "Location permission not granted, " + "showing default location",
+                contextActivity, "Location permission not granted, " + "showing default location",
                 Toast.LENGTH_SHORT
             ).show()
 
@@ -103,8 +86,8 @@ class MapsActivity : AppCompatActivity(),
             mMap.isMyLocationEnabled = true
             mMap.setOnMyLocationButtonClickListener(this)
             mMap.setOnMyLocationClickListener(this)
-
-            var locationManager : LocationManager =  getSystemService(LOCATION_SERVICE) as LocationManager
+            //contextActivity.
+            var locationManager : LocationManager = getActivity()!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager//ContextCompat.getSystemService(contextActivity!!, LOCATION_SERVICE) as LocationManager
             var bestProvider : String = locationManager.getBestProvider(Criteria(), true)
             var location = locationManager.getLastKnownLocation(bestProvider);
             if (location != null) {
@@ -117,7 +100,7 @@ class MapsActivity : AppCompatActivity(),
 
     fun showDefaultLocation() {
         Toast.makeText(
-            this, "Location permission not granted, " + "showing default location",
+            contextActivity, "Location permission not granted, " + "showing default location",
             Toast.LENGTH_SHORT
         ).show()
         val redmond = LatLng(47.6739881, -122.121512)
@@ -192,15 +175,15 @@ class MapsActivity : AppCompatActivity(),
     }
 
     override fun onProviderEnabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //Method Override
     }
 
     override fun onProviderDisabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //Method Override
     }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //Method Override
     }
 
 
@@ -208,11 +191,11 @@ class MapsActivity : AppCompatActivity(),
     //Google playServices
     private fun isGooglePlayServicesAvailable(): Boolean {
         val googleApiAvailability = GoogleApiAvailability.getInstance()
-        val status = googleApiAvailability.isGooglePlayServicesAvailable(this)
+        val status = googleApiAvailability.isGooglePlayServicesAvailable(contextActivity)
         if (ConnectionResult.SUCCESS == status)
             return true
         else  if (googleApiAvailability.isUserResolvableError(status)){
-            var dialog = GooglePlayServicesUtil.getErrorDialog(status, this, status);
+            var dialog = GooglePlayServicesUtil.getErrorDialog(status, getActivity(), status);
             dialog.show();
         }
         return false
